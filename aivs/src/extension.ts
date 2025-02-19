e7c5587c86e83f63cfb9c919123dbca4d64d777a
= <=>
2025-02-20 01:06:32 +0200

    🤷🏻

--- aivs/src/extension.ts
+++ aivs/src/extension.ts
@@ -1,11 +1,9 @@
 import * as vscode from 'vscode';
 import ollama from 'ollama';
-import { marked } from 'marked';
 
 export function activate(context: vscode.ExtensionContext) {
-	let modelName = 'codellama:latest';
-	vscode.window.showInformationMessage('AIVS is Installed.');
-	const messages: {role:string, content:string}[] = []; // todo: load from config?
+	//vscode.window.showInformationMessage('AIVS is Installed.');
+	const messages: {role:string, content:string}[] = []; // todo: load from file?
 	
 	const disposables = [
 		vscode.commands.registerCommand('aivs.killyourself', () => {
@@ -14,7 +12,7 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.commands.registerCommand('aivs.openTelegramWeb', () => {
 			vscode.window.showErrorMessage('Sorry Dave, but I can not do that.');
 		}),
-		vscode.commands.registerCommand('aivs.startChat', async () => {
+		vscode.commands.registerCommand('aivs.openChat', async () => {
 			const panel = vscode.window.createWebviewPanel(
 				'panel',
 				'AIVS',
@@ -23,20 +21,17 @@ export function activate(context: vscode.ExtensionContext) {
 			);
 			panel.webview.html = await buildChatWebViewHtml(context);
 			panel.webview.onDidReceiveMessage(async (msg) => {
-				//showPs(modelName);
 				if (msg.command === 'chat') {
 					let responseText = '';
 					try {
 						messages.push({
 							role: 'user',
 							content: msg.text
-						});
+						})
 						const streamResponse = await ollama.chat({
-							model: modelName,
+							model: 'codellama',
 							stream: true,
-							messages,
-							// format: 'json', // this will make model to answer in random json format (or in particluar json scheme if given)
-							
+							messages
 						});
 						let role = 'system';
 						for await (const part of streamResponse) {
@@ -51,14 +46,11 @@ export function activate(context: vscode.ExtensionContext) {
 							content: responseText,
 							role
 						});
-						const html = marked.parse(responseText);
 						panel.webview.postMessage({
 							command: 'chatFinishResponse',
-							text: html
+							text: responseText
 						});
-						showPs(modelName);
-						
-						// todo: preserve messages somehow?
+						console.debug(messages);
 					} catch (err) {
 						vscode.window.showErrorMessage((err as Error).message);
 					}
@@ -83,27 +75,4 @@ async function buildChatWebViewHtml(context: vscode.ExtensionContext) {
 		console.error('Error reading HTML file:', error);
 		return "<h1>Error loading HTML</h1>"; // Error handling
 	}
-}
-
-
-async function showPs(modelName: string) {
-	const ps = await ollama.ps();
-				//console.table(ps.models);
-				ps.models.some(model => {
-					if (model.name === modelName) {
-						// todo: figure out how to better present this information
-						vscode.window.showInformationMessage(
-							
-							`Name: ${model.name}`,
-							`Size: ${model.size}`,
-							`RAM Size: ${model.size_vram}`,
-							`Details:`,
-							`${JSON.stringify(model.details)}`
-							
-						);
-						return true;
-					} else {
-						return false;
-					}
-				});
 }
